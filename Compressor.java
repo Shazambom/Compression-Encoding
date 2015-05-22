875e9c145b1ef5ba6fea5ceff06e2f7d29e32e3d
Shazambom <mrderp14@gmail.com>
2015-05-22 14:33:23 -0400

    Completely Fixed Runline
    Restructured Runline completely. Now it just use the ~ for a flag
    always I think. I think this is more efficient and it has less of an
    overhead. Also tweaked compressor a little bit. I think I am going to
    put some sort of menu system in the Compressor class… but not until I
    get LZ77 working. I also have to get LZ77 working with Huffman… I will
    have to make a new version of Huffman that is slightly different.

--- Compressor.java
+++ Compressor.java
@@ -9,20 +9,22 @@ public class Compressor {
      * @param args the first arg will be the name of the file path to the file to be compressed
      */
     public static void main(String[] args) {
-        int times = 1;
+        int hTimes = 1;
+        int rlTimes = 0;
+        int lzTimes = 0;
 //        File toCompress = new File(args[0]);
         File toCompress = new File("TestFiles/big.txt");
 
-        for (int i = 0; i < times; i++) {
+        for (int i = 0; i < hTimes; i++) {
             toCompress = Huffman.encode(toCompress);
         }
-        for (int i = 0; i < times; i++) {
+        for (int i = 0; i < rlTimes; i++) {
             toCompress = Runline.encode((toCompress));
         }
-        for (int i = 0; i < times; i++) {
+        for (int i = 0; i < rlTimes; i++) {
             toCompress = Runline.decode(toCompress);
         }
-        for (int i = 0; i < times; i++) {
+        for (int i = 0; i < hTimes; i++) {
             toCompress = Huffman.decode(toCompress);
         }
 
--- Runline.java
+++ Runline.java
@@ -4,19 +4,17 @@
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.nio.ByteBuffer;
 import java.nio.file.Files;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.ArrayList;
 
 public class Runline extends Encoder {
     private static final ArrayList<Byte> BYTEDECK = genByteDeck();
+    private static final byte FLAG = BYTEDECK.get(0);
 
     public static void main(String[] args) {
-        int times = 1;
+        int times = 5;
 //        File encoded = encode(new File(args[0]));
-        File encoded = encode(new File("TestFiles/gon.png"));
+        File encoded = encode(new File("TestFiles/0s.txt"));
 
         for (int i = 0; i < times - 1; i++) {
             encoded = encode(encoded);
@@ -46,53 +44,36 @@ public class Runline extends Encoder {
         File toReturn = null;
 
         try {
-            System.out.print("Initializing flags: ");
+            System.out.print("Reading File: ");
             encodedBitStream = new ArrayList<Byte>();
             nonEncodedBytes = Files.readAllBytes(toEncode.toPath());
-            int overflowBytes = 0;
-            if (nonEncodedBytes.length % 255 != 0) {
-                overflowBytes++;
-            }
-            flags = new byte[(nonEncodedBytes.length / 255) + overflowBytes];
-            genFlags(flags, nonEncodedBytes);
-
-            System.out.println("\n");
-            for (byte flag: flags) {
-                System.out.println(String.format("%8s", Integer.toBinaryString(flag & 0xFF)).replace(' ', '0'));
-            }
-
-            byte[] size = ByteBuffer.allocate(4).putInt(flags.length).array();
-            for (byte element: size) {
-                encodedBitStream.add(element);
-            }
-
-            System.out.println(flags.length + " Flags Completed");
+            System.out.println("Completed");
             System.out.print("Encoding Bytes: ");
 
-
-            for (int i = 0; i < flags.length; i++) {
-                encodedBitStream.add(flags[i]);
-                byte saveByte = nonEncodedBytes[i * 255];
-                int numBytes = 1;
-                System.out.println(encodedBitStream.size() - 1);
-                for (int j = 1; j <= 255
-                        && j + (i * 255) < nonEncodedBytes.length; j++) {
-                    if (saveByte == nonEncodedBytes[j + (i * 255)] && numBytes < 255) {
-                        numBytes++;
-                    } else {
-                        if (numBytes > 3 || numBytes >= 255) {
-                            encodedBitStream.add(flags[i]);
-                            encodedBitStream.add(BYTEDECK.get((numBytes - 1)));
-                            encodedBitStream.add(saveByte);
-                        } else {
-                            for (int k = 0; k < numBytes; k++) {
-                                encodedBitStream.add(saveByte);
-                            }
-                        }
-                        numBytes = 1;
-                        saveByte = nonEncodedBytes[j + (i * 255)];
+            for (int i = 0; i < nonEncodedBytes.length; i++) {
+                byte saveByte = nonEncodedBytes[i];
+                boolean isFlag = false;
+                if (saveByte == FLAG) {
+                    isFlag = true;
+                }
+                int count = 1;
+                while (i < nonEncodedBytes.length - 1 && saveByte == nonEncodedBytes[i + 1]) {
+                    count++;
+                    i++;
+                    if (count >= 255) {
+                        break;
                     }
                 }
+                if (count > 3 || isFlag) {
+                    encodedBitStream.add(FLAG);
+                    encodedBitStream.add(BYTEDECK.get(count - 1));
+                    encodedBitStream.add(saveByte);
+                } else {
+                    for (int j = 0; j < count; j++) {
+                        encodedBitStream.add(saveByte);
+                    }
+                }
+
             }
             System.out.println("Completed");
 
@@ -140,43 +121,28 @@ public class Runline extends Encoder {
             System.out.print("Reading Bytes: ");
             byte[] bytes =  Files.readAllBytes(toDecode.toPath());
             System.out.println("Completed");
-            int numChunks = ByteBuffer.wrap(bytes).getInt(0);
             ArrayList<Byte> decodedStream = new ArrayList<Byte>();
-            byte flag = bytes[4];
-            int loc = 5;
             System.out.print("Decoding Stream: ");
-            System.out.println("\n");
-            for (int i = 0; i < numChunks; i++) {
-                System.out.println(String.format("%8s", Integer.toBinaryString(flag & 0xFF)).replace(' ', '0'));
-                for (int j = 0; j < 255 && loc < bytes.length; j++) {
-                    if (bytes[loc] == flag && loc + 3 < bytes.length) {
-                        byte numBytes = bytes[loc + 1];
-                        byte data = bytes[loc + 2];
-                        int num = 0;
-                        for (int l = 0; l < BYTEDECK.size(); l++) {
-                            if (numBytes == BYTEDECK.get(l)) {
-                                num = l + 1;
-                                break;
-                            }
+            for (int i = 0; i < bytes.length; i++) {
+                if (bytes[i] == FLAG && i + 3 < bytes.length) {
+                    byte countByte = bytes[i + 1];
+                    byte data = bytes[i + 2];
+                    i += 2;
+                    int count;
+                    for (count = 0; count < BYTEDECK.size(); count++) {
+                        if (countByte == BYTEDECK.get(count)) {
+                            count++;
+                            break;
                         }
-
-                        for (int k = 0; k < num; k++) {
-                            decodedStream.add(data);
-                        }
-                        loc += 3;
-                        j += num - 1;
-                    } else {
-                        decodedStream.add(bytes[loc]);
-                        loc++;
                     }
-                }
-
-                if (loc < bytes.length) {
-                    System.out.println(loc);
-                    flag = bytes[loc];
-                    loc++;
+                    for (int j = 0; j < count; j++) {
+                        decodedStream.add(data);
+                    }
+                } else {
+                    decodedStream.add(bytes[i]);
                 }
             }
+
             System.out.println("Completed");
             toReturn = new File("Decoded.txt");
             System.out.print("Writing to file: ");
@@ -196,28 +162,6 @@ public class Runline extends Encoder {
         return toReturn;
     }
 
-    /**
-     * Creates the flag for every 255 bytes
-     * @param chunk the chunk of 255 bytes that a flag gonna be generated for
-     * @throws Exception throws an exception if there is no key to be found
-     * @return the flag for that chunk of bytes
-     */
-    private static byte genFlag(byte[] chunk)
-            throws Exception {
-        HashMap<Byte, Integer> flagTable = new HashMap<Byte, Integer>();
-        for (byte deck: BYTEDECK) {
-            flagTable.put(deck, 0);
-        }
-        for (byte element: chunk) {
-            flagTable.replace(element, flagTable.get(element) + 1);
-        }
-        for (Map.Entry<Byte, Integer> element: flagTable.entrySet()) {
-            if (element.getValue() == 0) {
-                return element.getKey();
-            }
-        }
-        throw new Exception("Couldn't Find a key");
-    }
 
     /**
      * Generates the Byte deck with Nodes for the priority queues
@@ -236,33 +180,6 @@ public class Runline extends Encoder {
         return toReturn;
     }
 
-    /**
-     * Generates the flags for the chunks
-     * @param flags the array where the flags are to be added
-     * @param bitStream the stream of bits from the file
-     */
-    private static void genFlags(byte[] flags, byte[] bitStream) {
-        try {
-            int loc = 0;
-            ArrayList<Byte> chunkArrayList = new ArrayList<Byte>();
-            for (int i = 0; i < flags.length; i++) {
-                for (int j = 0; j < 255; j++) {
-                    if (loc + j < bitStream.length) {
-                        chunkArrayList.add(bitStream[loc + j]);
-                    }
-                }
-                byte[] chunk = new byte[chunkArrayList.size()];
-                for (int j = 0; j < chunk.length; j++) {
-                    chunk[j] = chunkArrayList.get(j);
-                }
-                flags[i] = genFlag(chunk);
-                chunkArrayList.clear();
-                loc += 255;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
 
 }
